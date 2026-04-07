3b3811123977db266587663bf1b104573c3e2383
asvg05 <adrian.stephen.gabor@student.ateneo.edu>
2026-04-07 20:09:54 +0800

    Added login and signup template

--- tapasV2/tapas/tapasapp/views.py
+++ tapasV2/tapas/tapasapp/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Dish 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.contrib.auth.models import User
+from .forms import SignupForm
+from .models import Dish
 
 # Create your views here.
 
+def login_form(request):
+    error_message = None
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('better_menu')
+        else:
+            error_message = "Invalid Credentials"
+    return render(request, 'tapasapp/login.html', {'error':error_message})
+
+def signup_form(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = User.objects.create_user(username = username, password = password)
+            login(request, user)
+            return redirect('better_menu')
+    else:
+        form = SignupForm()
+    return render(request, 'tapasapp/signup.html', {'form':form})        
 
 def better_menu(request):
     dish_objects = Dish.objects.all()
@@ -27,11 +58,11 @@ def delete_dish(request, pk):
     return redirect('better_menu')
 
 def update_dish(request, pk):
-    if(request.method=="POST"):
+    if request.method == "POST":
         cooktime = request.POST.get('ctime')
         preptime = request.POST.get('ptime')
         Dish.objects.filter(pk=pk).update(cook_time=cooktime, prep_time=preptime)
         return redirect('view_detail', pk=pk)
     else:
         d = get_object_or_404(Dish, pk=pk)
-        return render(request, 'tapasapp/update_menu.html', {'d':d})
+        return render(request, 'tapasapp/update_menu.html', {'d': d})
